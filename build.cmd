298e367a3b1a91fffd430392455a321b9a971ff6
Atif Aziz <code@raboof.com>
2014-07-11 14:35:58 +0200

    Nuspec and packaging scripts

--- build.cmd
+++ build.cmd
@@ -1,16 +1,15 @@
 @echo off
 setlocal
-pushd "%~dp0"
-call :main %*
-popd
-goto :EOF
-
-:main
-set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
-if not exist "%MSBUILDEXE%" (
+cd "%~dp0"
+set MSBUILD=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+if not exist "%MSBUILD%" (
     echo The .NET Framework 4.0 does not appear to be installed on this
     echo machine, which is required to build the solution.
     exit /b 1
 )
-set EnableNuGetPackageRestore=true
-for %%s in (*.sln) do for %%c in (debug release) do "%MSBUILDEXE%" %%s /p:Configuration=%%c /v:m %*
+call :msbuild Debug %* && call :msbuild Release %*
+goto :EOF
+
+:msbuild
+"%MSBUILD%" OpenWebClient.sln "/p:Configuration=%~1" /v:m %2 %3 %4 %5 %6 %7 %8 %9
+goto :EOF
